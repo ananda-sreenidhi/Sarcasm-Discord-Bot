7229419ae765e8bc1b572d22b39bafd65a2561d8
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-04-27 18:58:13 +0530

    Add files via upload

---
+++ boop.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import os
+
+token = open("token.txt", "r").read()
+client = commands.Bot(command_prefix = "!")
+
+initial_extensions = ['cogs.sarc']
+
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for extension in initial_extensions:
+    client.load_extension(extension)
+
+client.run(token)
